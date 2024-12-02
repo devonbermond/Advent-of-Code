8a243f1de0b2a4a729c6059335196ee57dd45096
BT_4N2_14 <sample@bths.edu>
2024-12-02 13:15:09 -0500

    Part 1 complete

---
+++ Day1.py
@@ -0,0 +1,23 @@
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+
+file_data = get_file_data("input.txt")
+list1 = []
+list2 = []
+total = 0
+for item in file_data:
+    temp = item.split("   ")
+    list1.append(temp[0])
+    list2.append(temp[1])
+list1.sort()
+list2.sort()
+x = 0;
+for item in list1:
+    total = total + abs(int(item) - int(list2[x]))
+    x = x + 1
+print(total)
