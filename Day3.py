795483739266a988b15c9441c4b99ade7f055b58
devonbermond <145681561+devonbermond@users.noreply.github.com>
2024-12-10 13:21:22 -0500

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -7,16 +7,25 @@ def get_file_data(file_name):
         data.append(line.rstrip())
     return data
 
-
 file_data = get_file_data("input.txt")
+def on_or_off(string, index):
+    str = string.substring(0, index)
+    temp = str[::-1]
+    do = temp.find("do()")
+    dont = temp.find("dont()")
+    return bool(do < dont)
+
 split = ""
 regex = "mul\\([0-9]+,[0-9]+\\)"
 for i in file_data:
     matches = re.findall(regex, i)
     for item in matches:
-        split = item.lstrip("mul(")
-        split = split.rstrip(")")
-        nums = split.split(",")
-        total = total + int(nums[0]) * int(nums[1])
+        idx = i.index(item)
+        idk = on_or_off(i, idx)
+        if (idk is True ):
+            split = item.lstrip("mul(")
+            split = split.rstrip(")")
+            nums = split.split(",")
+            total = total + int(nums[0]) * int(nums[1])
 
 print(total)
