e322bcd8c13d31d24c86fc237436245030d9b4c8
devonbermond <145681561+devonbermond@users.noreply.github.com>
2024-12-12 13:07:02 -0500

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,6 +1,6 @@
 import re
 total = 0
-vqar = True
+var = True
 def get_file_data(file_name):
     f = open(file_name)
     data = []
@@ -9,13 +9,6 @@ def get_file_data(file_name):
     return data
 
 file_data = get_file_data("input.txt")
-def on_or_off(string, index):
-    str = string[:index]
-    temp = str[::-1]
-    do = temp.find(")(od")
-    dont = temp.find(")(t'nod")
-    return bool(do < dont)
-
 
 def get_nums(str):
     temp = str.lstrip("mul(")
@@ -24,13 +17,15 @@ def get_nums(str):
     return num
 
 
-regex = "mul\\([0-9]+,[0-9]+\\)"
+regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
 for i in file_data:
     matches = re.findall(regex, i)
     for item in matches:
-        idx = i.index(item)
-        vqar = on_or_off(i, idx)
-        if vqar is True:
+        if item == "do()":
+            var = True
+        elif item == "don't()":
+            var = False
+        elif var is True:
             nums = get_nums(item)
             total = total + int(nums[0]) * int(nums[1])
 
