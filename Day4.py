ae790fcb12464568269d7e6b49b4aaef336550b5
devonbermond <145681561+devonbermond@users.noreply.github.com>
2024-12-12 13:20:09 -0500

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,29 @@
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+file_data = get_file_data("input.txt")
+
+def horizontalSearch(data):
+    print()
+
+def reverseSearch(data):
+    print()
+
+def VerticalSearchUp
+
+def diagonalSearchLeftUp(data):
+    print()
+
+def diagonalSearchRightUp(data):
+    print()
+
+def diagonalSearchLeftDown(data):
+    print()
+
+def diagonalSearchRightDown(data):
+    print()
+
