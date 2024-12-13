c965639eaf0834d11e1a2df5e23b6b9fe07a8381
devonbermond <145681561+devonbermond@users.noreply.github.com>
2024-12-13 13:19:38 -0500

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,3 +1,4 @@
+total = 0
 def get_file_data(file_name):
     f = open(file_name)
     data = []
@@ -6,14 +7,28 @@ def get_file_data(file_name):
     return data
 
 file_data = get_file_data("input.txt")
+puzzle = []
+for i in file_data:
+    array = []
+    for char in i:
+        array.append(char)
+    puzzle.append(array)
+
 
 def horizontalSearch(data):
-    print()
+    for row in data:
+        for ele in row:
+            if data[row][ele] == "X" and data[row + 1][ele] and "M" and data[row + 2][ele] == "A" and data[row + 3][ele] == "S":
+                total = total + 1
 
 def reverseSearch(data):
     print()
 
-def VerticalSearchUp
+def verticalSearchUp(data):
+    print()
+
+def verticalSearchDown(data):
+    print()
 
 def diagonalSearchLeftUp(data):
     print()
@@ -26,4 +41,3 @@ def diagonalSearchLeftDown(data):
 
 def diagonalSearchRightDown(data):
     print()
-
